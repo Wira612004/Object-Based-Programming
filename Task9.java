b900c09d06b82e7d9811c7c8b0bb93bf08364b03
Wira612004 <114727898+Wira612004@users.noreply.github.com>
2024-05-17 06:06:15 +0700

    Task9.java

--- Task9.java
+++ Task9.java
@@ -1,1 +1,15 @@
+// Wira Harsa - TI22I
+// 1.
+public class Task9 {
+    public static void main(String[] args) {
+        String name = "WIRAHARSA";
 
+        for (int i = 0; i <= name.length(); i++) {
+            String stars = "";
+            for (int j = 0; j < i; j++) {
+                stars += "*";
+            }
+            System.out.println(name.substring(0, name.length() - i) + stars);
+        }
+    }
+}
